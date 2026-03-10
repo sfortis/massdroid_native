355b0426539d812459567c4dc68c0590466506f0
sfortis <sfortis@gmail.com>
2026-03-10 14:54:19 +0200

    v2.2.0: Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "net.asksakis.massdroidv2"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.1.0"
+        versionCode = 5
+        versionName = "2.2.0"
     }
 
     signingConfigs {
